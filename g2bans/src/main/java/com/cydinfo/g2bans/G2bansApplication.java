cb6da2e221823189565767ab319bbbf48ed7be31
leeWonJong <lwjong99@gmail.com>
2024-07-29 15:33:49 +0900

    First Commit

---
+++ g2bans/src/main/java/com/cydinfo/g2bans/G2bansApplication.java
@@ -0,0 +1,13 @@
+package com.cydinfo.g2bans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2bansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2bansApplication.class, args);
+	}
+
+}
---
+++ g2bans/src/test/java/com/cydinfo/g2bans/G2bansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydinfo.g2bans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2bansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
